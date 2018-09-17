adb677f7783021de8c40412b4de721b203127f75
Paulo Scardine <paulo@telnyx.com>
2018-09-16 22:48:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# Nylas Coding Challenge
+
+This is the assignment:
+
+>Developers have been reporting flapping ports on various services in a cluster that you manage. Sometimes they see their services as up, and sometimes they’re down, with no clear reason as to why. They’ve asked you to diagnose the issue and given you a list of hosts to check. You’ve already vetted the network and don’t think it’s at fault.
+>
+>Write a script, in the language of your choice, that will take a list of hosts on STDIN, one per line, and a TCP port number as a command-line parameter. Your script will need to work for 100/1000/10000 hosts across multiple data centers and subnets.
+>
+>Your program should report on:
+> * Hosts where the TCP port is up one minute and down another
+> * Hosts that are down. A host is considered ‘down’ if it will not accept a TCP connection 5 times in a row
+>
+> Given the output of this script
+> * How would you deploy this as a script/service/etc?
+> * How would you work with developers to resolve any systems issues?
+> * What, if any, would the impact be of your script on the production service?
+> 
+> Example call of this script:
+>
+>     $ python flappy-ports.py 8080 <<EOF
+>     127.23.12.67
+>     sync-124.nylas.net
+>     sync-166.nylas.net
+>     sync-224.nylas.net
+>     EOF
+>     127.23.12.67 is down
+>     sync-124.nylas.net is flappy
+>     sync-166.nylas.net is flappy
+>     sync-224.nylas.net is reporting ok
+> 
+> Notes:
+> * you can use the language of your choice for this problem
+> * you can assume that the code is going to run in a Linux environment
+> * it is up to you to judge the frequency window of how often ports should be checked
+> * try not to spend more than a couple hours on this problem
+> * again, don’t hesitate to let me know if you have any questions!
+
+## My Solution
+
+I'm using Python's `asyncio.open_connection` from 3.6 in order to have some sort of 
+concurrency (but the script is doing host name resolution synchronously upfront so it
+may take a while to start). I'm also using RRD to record ping results. If you omit the
+`--input_file` argument it will read from standard input.
+
+    $ python3 pingport.py --help
+    
+    usage: pingport.py [-h] --port PORT [--rrd RRD] [--interval INTERVAL]
+                       [--timeout TIMEOUT] [--input_file INPUT_FILE] [--verbose]
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      --port PORT           TCP port
+      --rrd RRD             RRD Database path, default ./rrd
+      --interval INTERVAL   Ping interval (default 600)
+      --timeout TIMEOUT     Timeout in seconds (default 300)
+      --input_file INPUT_FILE
+                            Input file, default stdin
+      --verbose             Show progress messages
+      
+Example:
+
+    python3 pingport.py --input_file=sample-data/random-1000-ips.txt --port 80 --verbose
+
+You may have to adjust `--interval` and `--timeout` according to your network speed and
+the size of your host list - the defaults are pretty conservative with 5 minutes between
+pings and 30 second timeouts. The sample domain names and ips in the `sample-data` folder
+are taken from the [citzenlab/test-list](https://github.com/citizenlab/test-lists) repo.
+
+## Install
+
+This was tested in a Macbook using Python 3.6. You need rrdtool, Python headers and a 
+working C compiler.
+ 
+If you are using a Mac, do:
+
+    brew install rrdtool
+    
+On Debian/Ubuntu do:
+
+    sudo apt-get install librrd-dev libpython3-dev
+    
+On RHEL/CentOS:
+
+    sudo yum install rrdtool-devel python34-devel
+    
+Clone this repo at 
+After installing the dependencies you can fire `pip` (you may want to create a [virtual
+environment](https://docs.python.org/3/library/venv.html), I will not cover this step here):
+
+    pip install --user -r requirements.txt
+    
+    
+    
+## Why Python
+
+Well, Python is likely to be present on any Linux machine so it is a great tool for fast 
+prototyping and making ad hoc scripts. Before Python, Perl used to be the swiss knife of
+choice for the network administrator - but Python is an order of magnitude easier to 
+maintain (I kid you not, sometimes I had trouble understanding my own scripts a couple 
+weeks after I wrote them).
+   
+## Why RRD?
+
+I like to log results but don't like to deal with log rotation; for network monitoring
+RRD databases fit the bill as they have deterministic size. Also, you can easily 
+create nice charts from them later.
---
+++ pingport.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+import argparse
+import asyncio
+import os
+import re
+import socket
+
+import async_timeout
+import rrdtool
+import sys
+
+DEFAULT_TIMEOUT = 30
+DEFAULT_INTERVAL = 300  # 5 minutes
+RRD_FNAME_CACHE = {}
+IP_CACHE = {}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", help="TCP port", type=int, required=True)
+    parser.add_argument("--rrd", help="RRD Database path, default ./rrd", type=str, default="rrd")
+    parser.add_argument(
+        "--interval",
+        help=f"Ping interval (default {DEFAULT_INTERVAL})",
+        type=int,
+        default=DEFAULT_INTERVAL
+    )
+    parser.add_argument(
+        "--timeout",
+        help=f"Timeout in seconds (default {DEFAULT_TIMEOUT})",
+        type=int,
+        default=DEFAULT_TIMEOUT
+    )
+    parser.add_argument("--input_file", help="Input file, default stdin", default='-')
+    parser.add_argument("--verbose", help="Show progress messages", action='store_true')
+    return parser.parse_args()
+
+
+def host2filename(argv, host):
+    global RRD_FNAME_CACHE
+
+    if host in RRD_FNAME_CACHE:
+        return RRD_FNAME_CACHE[host]
+    parts = re.split(r"\W", IP_CACHE[host])
+    parts[-1] +=  ".rrd"
+    if not os.path.isdir(os.path.join(argv.rrd, *parts[:-1])):
+        os.makedirs(os.path.join(argv.rrd, *parts[:-1]), exist_ok=True)
+    rrd = os.path.join(argv.rrd, *parts)
+    RRD_FNAME_CACHE[host] = rrd
+    return rrd
+
+
+def main():
+    argv = parse_args()
+    if argv.input_file == '-':
+        hosts = [line.strip() for line in sys.stdin if line]
+    else:
+        with open(argv.input_file) as input_file:
+            hosts = [line.strip() for line in input_file if line]
+
+    if argv.verbose:
+        print(f"Resolving {len(hosts)} names...")
+
+    global IP_CACHE
+    for i, host in enumerate(hosts):
+        try:
+            IP_CACHE[host] = socket.gethostbyname(host)
+        except socket.gaierror:
+            sys.stderr.write(f"Unable to resolve {host}, skipping...\n")
+        if argv.verbose and i and i % 10 == 0:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+    else:
+        if argv.verbose:
+            print(" OK!")
+
+    os.makedirs("rrd", exist_ok=True)
+    for host in hosts:
+        if host not in IP_CACHE:
+            continue
+        rrdtool.create(
+            host2filename(argv, host),
+            "--step", str(argv.interval),
+            "--start", "0",
+            f"DS:connect:GAUGE:{int(argv.interval + 2 * argv.timeout)}:0:U",
+            f"DS:time:GAUGE:{int(argv.interval + 2 * argv.timeout)}:0:U",
+            f"RRA:AVERAGE:0.5:1:1d",
+            f"RRA:AVERAGE:0.5:5:5d",
+            f"RRA:AVERAGE:0.5:60:120",
+            f"RRA:AVERAGE:0.5:420:840",
+        )
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(argv, hosts, loop))
+
+
+async def pingport(loop: asyncio.AbstractEventLoop, host: str, interval: int, argv):
+    started = loop.time()
+    elapsed = connected = 0
+    if host != IP_CACHE[host]:
+        hostname = f"{host}:{argv.port} ({IP_CACHE[host]})"
+    else:
+        hostname = f"{host}:{argv.port}"
+
+    with async_timeout.timeout(argv.timeout + 1):
+        try:
+            conn = asyncio.open_connection(IP_CACHE[host], argv.port, loop=loop)
+            reader, writer = await asyncio.wait_for(conn, timeout=argv.timeout)
+        except KeyboardInterrupt:
+            print("Ok, boss, lets call it a day.")
+            sys.exit(0)
+        except Exception as e:
+            if argv.verbose:
+                print(f"Ping {hostname} failed... ({e.__class__.__name__}: {str(e)!r})")
+        else:
+            if argv.verbose:
+                print(f"Ping {hostname} OK...")
+            connected = argv.timeout
+            elapsed = loop.time() - started
+
+    db = host2filename(argv, host)
+    # Check previous pings
+    if not connected:
+        last_update = rrdtool.lastupdate(db)
+        if 'ds' in last_update and last_update['ds']['connect']:
+            sys.stderr.write(f"{hostname} is flappy\n")
+        else:
+            last = rrdtool.fetch(db, "MIN", "--start", str(-argv.interval * 6))
+            last_connections = [int(c) for c, t in last[2] if c is not None][-5:]
+            if len(last_connections) == 5 and not any(last_connections):
+                sys.stderr.write(f"{hostname} is down\n")
+
+    # Record this attempt
+    rrdtool.update(db, f"N:{int(connected)}:{elapsed}",)
+    return await asyncio.sleep(interval)
+
+
+async def run(argv, hosts, loop: asyncio.AbstractEventLoop):
+    started = loop.time()
+    drift = i = 0
+    while True:
+        i += 1
+        interval = argv.interval - drift
+        tasks = [pingport(loop, host, interval, argv) for host in hosts if host in IP_CACHE]
+        await asyncio.gather(*tasks)
+        drift = loop.time() - i * argv.interval - started
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
